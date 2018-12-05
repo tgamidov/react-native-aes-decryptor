c480004cb52b890750bcff1feabb872da91c5fc4
Teymur Gamidov <tgamidov@riberry.az>
2018-12-06 03:11:49 +0400

    empty iv fix

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import { NativeModules } from 'react-native';
 const { RBRAesCrypto } = NativeModules;
 export default {
 	decryptAESFile(fSrc, fDst, secretKey, iv) {
-        if (!fSrc || !fDst || !secretKey || !iv) {
+        if (!fSrc || !fDst || !secretKey || iv === null || iv === undefined) {
             return Promise.reject(new Error("invalid parameter: fSrc, fDst, secretKey and iv are required"));
         }
 		RBRAesCrypto.decryptAESFile(fSrc, fDst, secretKey, iv);
--- ios/RBRAesCrypto/AesKit/NSData+AES.m
+++ ios/RBRAesCrypto/AesKit/NSData+AES.m
@@ -6,6 +6,14 @@
 @implementation NSData (Decryption)
 
 - (NSData *)AES128DecryptWithKey:(NSString *)key gIv:(NSString *)gIv {
+    if (gIv.length == 0) {
+        return [self AES128DecryptWithKeyOnly:key];
+    }
+    
+    return [self AES128DecryptWithKeyAndIv:key gIv: gIv];
+}
+
+- (NSData *)AES128DecryptWithKeyAndIv:(NSString *)key gIv:(NSString *)gIv {
     char keyPtr[kCCKeySizeAES128+1];
     bzero(keyPtr, sizeof(keyPtr));
     [key getCString:keyPtr maxLength:sizeof(keyPtr) encoding:NSUTF8StringEncoding];
@@ -14,8 +22,8 @@
     memset(ivPtr, 0, sizeof(ivPtr));
     [gIv getCString:ivPtr maxLength:sizeof(ivPtr) encoding:NSUTF8StringEncoding];
     
-    NSUInteger dataLength = [self length];
-    size_t bufferSize = dataLength + kCCBlockSizeAES128;
+    NSUInteger dataLength = [self length] - kCCBlockSizeAES128;
+    size_t bufferSize = dataLength;
     void *buffer = malloc(bufferSize);
     size_t numBytesDecrypted = 0;
     CCCryptorStatus cryptStatus = CCCrypt(kCCDecrypt,
@@ -23,8 +31,35 @@
                                           kCCOptionPKCS7Padding,
                                           keyPtr,
                                           kCCBlockSizeAES128,
-                                          ivPtr,
                                           [self bytes],
+                                          [self bytes] + kCCBlockSizeAES128,
+                                          dataLength,
+                                          buffer,
+                                          bufferSize,
+                                          &numBytesDecrypted);
+    if (cryptStatus == kCCSuccess) {
+        return [NSData dataWithBytesNoCopy:buffer length:numBytesDecrypted];
+    }
+    free(buffer);
+    return nil;
+}
+
+- (NSData *)AES128DecryptWithKeyOnly:(NSString *)key {
+    char keyPtr[kCCKeySizeAES128+1];
+    bzero(keyPtr, sizeof(keyPtr));
+    [key getCString:keyPtr maxLength:sizeof(keyPtr) encoding:NSUTF8StringEncoding];
+    
+    NSUInteger dataLength = [self length] - kCCBlockSizeAES128;
+    size_t bufferSize = dataLength;
+    void *buffer = malloc(bufferSize);
+    size_t numBytesDecrypted = 0;
+    CCCryptorStatus cryptStatus = CCCrypt(kCCDecrypt,
+                                          kCCAlgorithmAES128,
+                                          kCCOptionPKCS7Padding,
+                                          keyPtr,
+                                          kCCBlockSizeAES128,
+                                          [self bytes],
+                                          [self bytes] + kCCBlockSizeAES128,
                                           dataLength,
                                           buffer,
                                           bufferSize,
